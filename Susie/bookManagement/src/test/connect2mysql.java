094b2176f4fff7e49992ae6512b845e49d38479f
Susie Hu <si-qin.hu@hpe.com>
2016-12-16 15:31:41 +0800

    Revert "add DB operation"
    This reverts commit c6f58990765380f25b9bdf1d4afb23bac797d00c.

--- Susie/bookManagement/src/test/connect2mysql.java
+++ Susie/bookManagement/src/test/connect2mysql.java
@@ -7,41 +7,41 @@ import java.sql.*;
  */
 public class connect2mysql {
     public static void main(String args[]){
-//        System.out.println("********Start test Mysql*******");
-//        Connection con = null;
-//        String sqlUrl = "jdbc:mysql://127.0.0.1:3306/bookmanagment";
-//        String user = "test";
-//        String passwd = "test";
-//        String sqlQuery = "select * from user;";
-////        ? 获取MySQL驱动的实例
-//        try {
-////            反射
-//            Class.forName("com.mysql.jdbc.Driver").newInstance();
-//            con = DriverManager.getConnection(sqlUrl + "?user=" + user + "&password=" + passwd + "&useSSL=true"
-//                    + "&verifyServerCertificate=false");
-////            con = DriverManager.getConnection(sqlUrl+"/user",user,passwd);
-//            if (!con.isClosed()){
-//                System.out.println("--------------connected to DB Successfully -------------");
-//                Statement st = con.createStatement();
-//                ResultSet rs = st.executeQuery(sqlQuery);
-//                while(rs.next()){
-//                    System.out.println("########## id " + rs.getInt("id"));
-//                    System.out.println("########## name " + rs.getString("name"));
-//                    System.out.println("########## passwd " + rs.getString("passwd"));
-//                    System.out.println("########## client " + rs.getInt("client"));
-//                    System.out.println("########## admin " + rs.getInt("admin"));
-//                    System.out.println("########## seller " + rs.getInt("seller"));
-//                }
-//                con.close();
-//            }
-//        } catch (InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        System.out.println("********Start test Mysql*******");
+        Connection con = null;
+        String sqlUrl = "jdbc:MySQL://127.0.0.1/bookmanagment";
+        String user = "root";
+        String passwd = "password1";
+        String sqlQuery = "select * from user where id = 0";
+//        ? 获取MySQL驱动的实例
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(sqlUrl+"/user",user,passwd);
+            if (!con.isClosed()){
+                System.out.println("--------------connected to DB Successfully -------------");
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(sqlQuery);
+                while(rs.next()){
+                    System.out.println("########## id " + rs.getInt("id"));
+                    System.out.println("########## name " + rs.getString("name"));
+                    System.out.println("########## passwd " + rs.getString("passwd"));
+                    System.out.println("########## client " + rs.getInt("client"));
+                    System.out.println("########## admin " + rs.getInt("admin"));
+                    System.out.println("########## seller " + rs.getInt("seller"));
+                }
+                con.close();
+            }
+            else{
+                System.out.println("--------------failed to connected to DB -------------");
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
